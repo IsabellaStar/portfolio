45fc64adc70248837dd5cf9b7a98bb992427916d
IsabellaStar <isabella.starowicz12@gmail.com>
2022-01-10 19:24:15 +0100

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,40 @@
+import Link from 'next/link';
+import { withRouter } from 'next/router';
+
+function Navbar({ router }) {
+  const navs = [
+    { text: 'Home', href: '/', dataCy: 'homePageLink' },
+    { text: 'About', href: '/about', dataCy: 'aboutPageLink' },
+    { text: 'Portfolio', href: '/portfolio', dataCy: 'portfolioPageLink' },
+    { text: 'Contact', href: '/contact', dataCy: 'contactPageLink' },
+    // { text: '', href: '' },
+  ];
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <Link href="/">
+          <a className="logo">Lutfhi.</a>
+        </Link>
+
+        <ul className="nav-links">
+          {navs.map((nav) => (
+            <li data-cy={nav.dataCy} key={nav.href}>
+              <Link href={nav.href}>
+                <a
+                  className={`nav-item ${
+                    router.pathname == nav.href ? 'active' : ''
+                  }`}
+                >
+                  {nav.text}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default withRouter(Navbar);
